afbf61a9598541df976bbb927cc4109d7c2feb59
rajendra <rajendrasm99@gmail.com>
2024-10-11 16:40:15 +0530

    changes

---
+++ index.js
@@ -0,0 +1,10 @@
+function showPopup() {
+    document.getElementById('popup').style.display = 'flex';
+}
+
+function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+}
+function showAlert() {
+    alert("Please call or click on Whatsapp Icon and Drop the message to Manoj Jha");
+}
