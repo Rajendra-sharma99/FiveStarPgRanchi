23ab8367c7fcbadbba13481076d843d5bdfa2a57
rajendra <rajendrasm99@gmail.com>
2024-10-11 17:39:58 +0530

    changes

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-function showPopup() {
-    document.getElementById('popup').style.display = 'flex';
-}
 
-function closePopup() {
-    document.getElementById('popup').style.display = 'none';
-}
 function showAlert() {
     alert("Please call or click on Whatsapp Icon and Drop the message to Manoj Jha");
+}
+
+function showOverviewAlert() {
+    alert("This is Basically Male Pg that is managed by Manoj kumar Jha who is availlable in Harmu Hosung Colony For More Please connect with Given Number")
+}
+function showRulesAlert() {
+    alert("There are some rules to maintain professionalism. So maintain this So that Distrubance can't be come foe everyone... ")
+}
+
+function showVisitAlert() {
+    alert("Please visit my Pg wellcome for you we will show you all things....")
+}
+
+function showSecutyAlert(){
+    alert("We are providing best possible security like whole outside pg and hall are under in CCTV....")
+}
+
+function showCleanerAlert() {
+    alert("We are provide best possible clearning facilities like per day room cleaning etc....")
+}
+
+function showMedicalAlert() {
+    alert("We are providing Emergency medical facilities if someone are facing any problem")
 }
